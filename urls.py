c4258d04356042d92935e3e251649b7565312b3b
Shimon Rura <shimon@rura.org>
2011-03-09 20:17:35 -0500

    added an image to spice things up!

--- urls.py
+++ urls.py
@@ -1,4 +1,6 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, url, include
+from django.conf import settings
+import os
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -8,7 +10,7 @@ urlpatterns = patterns('',
     # Example:
     # (r'^mysite/', include('mysite.foo.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
@@ -18,4 +20,7 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
 
     (r'^polls/', include('mysite.polls.urls')),
+
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': os.path.join(settings.ROOT_PATH, 'static')}),
 )
